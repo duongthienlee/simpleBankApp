ea0c0a3e152442d862f4c9846464c3e3e2ba559b
duongthienlee <duongthienlee@gmail.com>
2018-08-06 15:02:34 +0300

    branh gh-page

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { store } from './store';
+import { HashRouter, Route } from "react-router-dom"
 
-
-const render = () => ReactDOM.render(<App />, document.getElementById('root'));
+const render = () => ReactDOM.render(<HashRouter><Route exact path="/" component={App} /></HashRouter>, document.getElementById('root'));
 render();
 registerServiceWorker();
 store.subscribe(render);
